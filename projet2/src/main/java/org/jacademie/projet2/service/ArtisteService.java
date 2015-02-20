613bf4cb2b1c535f9859655ede4eb12cc1b09da9
nbouherrou <nacerbouherrou@gmail.com>
2015-02-20 18:38:28 +0100

    Artiste Service

--- projet2/src/main/java/org/jacademie/projet2/service/ArtisteService.java
+++ projet2/src/main/java/org/jacademie/projet2/service/ArtisteService.java
@@ -1,5 +1,11 @@
 package org.jacademie.projet2.service;
 
-public class ArtisteService {
+import java.util.Collection;
+
+import org.jacademie.projet2.domain.Artiste;
+
+public interface ArtisteService {
+	
+	public Collection<Artiste> retrieveAllArtistes();
 
 }
--- projet2/src/main/java/org/jacademie/projet2/service/impl/ArtisteServiceImpl.java
+++ projet2/src/main/java/org/jacademie/projet2/service/impl/ArtisteServiceImpl.java
@@ -1,5 +1,48 @@
 package org.jacademie.projet2.service.impl;
 
-public class ArtisteServiceImpl {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jacademie.projet2.dao.ArtisteDao;
+import org.jacademie.projet2.domain.Artiste;
+import org.jacademie.projet2.service.ArtisteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ArtisteServiceImpl implements ArtisteService {
+	
+	private static Logger logger = LogManager.getLogger(ArtisteServiceImpl.class);
+	
+	@Autowired
+	private ArtisteDao artisteDao;
+	
+	@Override
+	@Transactional(readOnly=true)
+	public Collection<Artiste> retrieveAllArtistes() {
+		
+		logger.info("In retrieveAllArtistes (Service)");
+		
+		ArrayList<Artiste> list = new ArrayList<Artiste>(artisteDao.retrieveAllArtistes());
+		
+		Collections.sort(list, new Comparator<Artiste>(){
+			
+			public int compare(Artiste a1, Artiste a2){
+	 
+				return a1.getIdArtiste().compareTo(a2.getIdArtiste());
+	 
+			}
+	 
+		});
+		
+		logger.info("Out of retrieveAllArtistes (Service)");
+		
+		return list;
+	}
 
 }
