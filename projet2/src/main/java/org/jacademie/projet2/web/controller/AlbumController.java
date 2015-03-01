22e181baa2674c78739212515a82d316d6bf3eea
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2015-03-01 22:48:10 +0100

    Link Artistes->Albums

--- projet2/src/main/java/org/jacademie/projet2/web/controller/AlbumController.java
+++ projet2/src/main/java/org/jacademie/projet2/web/controller/AlbumController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jacademie.projet2.domain.Album;
+import org.jacademie.projet2.domain.Artiste;
 import org.jacademie.projet2.service.AlbumService;
+import org.jacademie.projet2.service.ArtisteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AlbumController {
@@ -18,9 +21,12 @@ public class AlbumController {
 	private static Logger logger = LogManager.getLogger(AlbumController.class);
 
 	@Autowired
-	private AlbumService albumService;
+	private 	AlbumService 	albumService;
 	
-	@RequestMapping(value = "/Albums", method = RequestMethod.GET)
+	@Autowired
+	private 	ArtisteService 	artisteService;
+	
+	@RequestMapping(value = "/AlbumsListe", method = RequestMethod.GET)
     public String displayAlbums(Model model) throws Exception {
 
 		logger.info("In displayAlbums (Controller)");
@@ -32,6 +38,24 @@ public class AlbumController {
 		model.addAttribute("albums", albums);
 
 		logger.info("Out displayAlbums (Controller)");
+		
+		return "albums";
+    }
+	
+	@RequestMapping(value = "/Albums", method = RequestMethod.GET)
+    public String displayArtisteAlbums(@RequestParam("id") Integer id, Model model) throws Exception {
+
+		logger.info("In displayArtisteAlbums (Controller)");
+		
+		logger.info("Artiste id = " + id);
+		
+		Artiste artiste = artisteService.findArtisteById(id);
+		
+		model.addAttribute("albums", artiste.getAlbums());
+		
+		artiste.getAlbums().forEach(e -> logger.info(e));
+
+		logger.info("Out displayArtisteAlbums (Controller)");
 		
 		return "albums";
     }
